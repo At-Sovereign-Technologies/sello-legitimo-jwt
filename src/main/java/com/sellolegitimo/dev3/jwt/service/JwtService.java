118ed0973421817fa1d6ab1bdd423e73f6eb08c3
Juan Martín Sánchez <140517935+Palmxl@users.noreply.github.com>
2026-04-06 15:06:06 -0500

    Update JwtService.java

--- src/main/java/com/sellolegitimo/dev3/jwt/service/JwtService.java
+++ src/main/java/com/sellolegitimo/dev3/jwt/service/JwtService.java
@@ -4,6 +4,7 @@ import java.util.Date;
 
 import javax.crypto.SecretKey;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import io.jsonwebtoken.Jwts;
@@ -12,10 +13,11 @@ import io.jsonwebtoken.security.Keys;
 @Service
 public class JwtService {
 
-    private final String SECRET = "supersecreto_supersecreto_supersecreto_123";
+    @Value("${jwt.secret}")
+    private String secret;
 
     private SecretKey getKey() {
-        return Keys.hmacShaKeyFor(SECRET.getBytes());
+        return Keys.hmacShaKeyFor(secret.getBytes());
     }
 
     public String generateToken(String cedula) {
@@ -23,7 +25,7 @@ public class JwtService {
         return Jwts.builder()
                 .setSubject(cedula)
                 .setIssuedAt(new Date())
-                .setExpiration(new Date(System.currentTimeMillis() + 86400000))
+                .setExpiration(new Date(System.currentTimeMillis() + 86400000)) // 1 día
                 .signWith(getKey())
                 .compact();
     }
